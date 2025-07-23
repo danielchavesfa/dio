e0150ce5dfec23efa757aee08d1a502e4109d25f
Daniel Chaves <danielchavesfa@hotmail.com>
2025-07-23 14:27:22 -0300

    feat: implementa validação, formatação e remoção da placa da lista de veículos

--- trilha-net-fundamentos-desafio/DesafioFundamentos/Models/Estacionamento.cs
+++ trilha-net-fundamentos-desafio/DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -27,7 +29,7 @@ namespace DesafioFundamentos.Models
                 return;
             }
 
-            veiculos.Add(placaDoVeiculo);
+            veiculos.Add(placaDoVeiculo.ToUpper());
         }
 
         public void RemoverVeiculo()
@@ -36,7 +38,15 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(placa))
+            {
+                Console.Clear();
+                Console.WriteLine("Por favor, digite a placa do veículo corretamente.");
+                this.RemoverVeiculo();
+                return;
+            }
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -46,13 +56,15 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0;
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal valorTotal = precoInicial + (precoPorHora * horas);
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa.ToUpper());
+
+                Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
